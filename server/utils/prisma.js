97679725bff92601959e9a0409c24f3c6651614c
manishhsuthar <manishsuthar.dev@gmail.com>
2026-04-03 02:06:54 +0530

    fix: configure Prisma 7 Postgres adapter (pg) to resolve server startup crash

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -1,11 +1,20 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 
 const globalForPrisma = global;
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const pool = globalForPrisma.prismaPool || new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.prismaPool = pool;
 }
 
 module.exports = prisma;
